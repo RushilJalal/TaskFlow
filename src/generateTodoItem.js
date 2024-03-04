98daf2c7d6e22d5fb9e9ecd991f5c720ff2a3faf
RushilJalal <rushiljalal@gmail.com>
2024-03-04 11:21:53 +0530

    generateTodoItem

---
+++ src/generateTodoItem.js
@@ -0,0 +1,11 @@
+function generateTodoItem(title, desc, dueDate, priority) {
+    return {
+        title: title,
+        description: desc,
+        dueDate: dueDate,
+        priority: priority,
+        checked: false
+    }
+}
+
+export default generateTodoItem;
--- src/todoGenerator.js
+++
@@ -1,5 +0,0 @@
-function generateTodoItem(title, desc, dueDate, priority) {
-    return {
-        
-    }
-}
