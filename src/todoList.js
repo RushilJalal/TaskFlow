1c76c1cdd5a3aaba61c7e658ff7a59d62425e978
RushilJalal <rushiljalal@gmail.com>
2024-03-06 14:42:58 +0530

    add default todoList

---
+++ src/todoList.js
@@ -0,0 +1,18 @@
+const todoList = [
+    {
+        "title": "Buy bread",
+        "description": "buy 10 bread",
+        "duedate": "2024-03-01",
+        "priority": "low",
+        "checked": false
+    },
+    {
+        "title": "Study notes",
+        "description": "CNP notes",
+        "duedate": "2024-03-02",
+        "priority": "high",
+        "checked": true
+    }
+]
+
+export default todoList;
