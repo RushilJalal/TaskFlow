a7d4b961ebccecd52563840596574904a8f737db
RushilJalal <rushiljalal@gmail.com>
2024-03-06 16:49:26 +0530

    add import statement

--- src/todoList.js
+++ src/todoList.js
@@ -1,3 +1,5 @@
+import createTodoItem from "./createTodoItem";
+
 const todoList = [
     createTodoItem("Buy bread", "buy 10 bread", "2024-03-01", "low"),
     createTodoItem("Study notes", "CNP notes", "2024-03-02", "high", [], true)
