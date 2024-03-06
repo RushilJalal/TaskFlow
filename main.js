417cde87314457bae85afcc48c084fbd45b6ba52
RushilJalal <rushiljalal@gmail.com>
2024-03-06 14:57:10 +0530

    add addTodoItemToList

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 import './style.css';
-import addTodoItemToFile from './src/addTodoItemtoFile';
-import generateTodoItem from './src/generateTodoItem';
+import addTodoItemToList from './src/addTodoItemToList';
 
-const item = generateTodoItem({
-  "title": "Buy bread",
-  "description": "buy 10 bread",
-  "duedate": "2024-03-01",
-  "priority": "low",
-  "checked": false
-});
-
-addTodoItemToFile(item);
+addTodoItemToList();
---
+++ src/addTodoItemToList.js
@@ -0,0 +1,12 @@
+import todoList from "./todoList";
+import generateTodoItem from "./generateTodoItem";
+
+export default function addTodoItemToList() {
+    const todoItem = generateTodoItem("Buy surf", "buy from campus store", "2024-03-01", high);
+    todoList.push(todoItem);
+
+    console.log(todoList);
+}
+
+
+
