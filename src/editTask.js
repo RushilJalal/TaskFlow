629d823b81a07d34c17d5c7d7a3c56f484fe1cf6
Rushil Jalal <rushiljalal@gmail.com>
2024-05-22 10:39:42 +0530

    edit task works

--- src/editTask.js
+++ src/editTask.js
@@ -1,62 +1,66 @@
-// simple js to control edit task form and dialog
+import { renderTodoList } from "./renderTodoList";
+import { todoList } from "./todoModel";
 
-import { renderTodoList } from "./renderTodoList"
-import { todoList } from "./todoModel"
+let currentSubmitHandler = null;
 
 export function showEditTaskDialog(index) {
-    const dialog = document.querySelector(".edit-task-dialog")
-    dialog.showModal()
+    const dialog = document.querySelector(".edit-task-dialog");
+    dialog.showModal();
 
-    dialog.style.display = "block"
+    dialog.style.display = "flex";
 
-    const taskName = document.querySelector(".task-name")
-    const taskDesc = document.querySelector(".task-desc")
-    const taskDate = document.querySelector(".date")
-    const taskPriority = document.querySelector(".edit-task-priority")
+    const taskName = document.querySelector(".task-name");
+    const taskDesc = document.querySelector(".task-desc");
+    const taskDate = document.querySelector(".date");
+    const taskPriority = document.querySelector(".edit-task-priority");
 
-    // set the values of the task to be edited
-    taskName.value = todoList[index].title
-    taskDesc.value = todoList[index].description
-    taskDate.value = todoList[index].dueDate
-    taskPriority.value = todoList[index].priority
+    // Set the values of the task to be edited
+    taskName.value = todoList[index].title;
+    taskDesc.value = todoList[index].description;
+    taskDate.value = todoList[index].dueDate;
+    taskPriority.value = todoList[index].priority;
 
-    const submitEditTaskFormButton = document.querySelector(".submit-edit-task")
-    submitEditTaskFormButton.addEventListener("click", () => {
-        const name = taskName.value
-        const desc = taskDesc.value
-        const date = taskDate.value
-        const priority = taskPriority.value
+    const submitEditTaskFormButton = document.querySelector(".submit-edit-task");
+
+    // Remove the current event listener if it exists
+    if (currentSubmitHandler) {
+        submitEditTaskFormButton.removeEventListener("click", currentSubmitHandler);
+    }
+
+    currentSubmitHandler = () => {
+        const name = taskName.value;
+        const desc = taskDesc.value;
+        const date = taskDate.value;
+        const priority = taskPriority.value;
 
         if (name === "") {
-            alert("Enter name")
-            return
+            alert("Enter name");
+            return;
         }
 
-        editTodoItem(index, name, desc, date, priority)
+        editTodoItem(index, name, desc, date, priority);
 
-        dialog.style.display = "none"
-        dialog.close()
+        dialog.style.display = "none";
+        dialog.close();
 
-        const taskList = document.querySelector(".task-list")
-        taskList.textContent = ""
-        renderTodoList()
+        const taskList = document.querySelector(".task-list");
+        taskList.textContent = "";
+        renderTodoList();
+    };
 
-    })
+    submitEditTaskFormButton.addEventListener("click", currentSubmitHandler);
 
-    const close = document.querySelector(".close-button")
+    const close = document.querySelector(".close-button");
     close.addEventListener("click", () => {
-        dialog.style.display = "none"
-        dialog.close()
-    })
-
+        dialog.style.display = "none";
+        dialog.close();
+    });
 }
 
-//sets the task parameters to the new values given by user as input
+// Set the task parameters to the new values given by user as input
 function editTodoItem(index, name, desc, date, priority) {
-    todoList[index].title = name
-    todoList[index].description = desc
-    todoList[index].dueDate = date
-    todoList[index].priority = priority
+    todoList[index].title = name;
+    todoList[index].description = desc;
+    todoList[index].dueDate = date;
+    todoList[index].priority = priority;
 }
-
-
